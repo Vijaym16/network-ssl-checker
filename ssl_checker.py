759aa6f9fba898326fe68c2ed7f1a3bc8582ce9f
Vijay M <mpvj8734@gmail.com>
2026-03-26 23:00:10 +0530

    initial ssl_checker simple python script

---
+++ ssl_checker.py
@@ -0,0 +1,10 @@
+import ssl
+import socket
+from datetime import datetime
+
+def get_ssl_info(hostname):
+    context = ssl.create_default_context()
+    with socket.create_connection((hostname, 443)) as sock:
+        with context.wrap_socket(sock, server_hostname=hostname) as ssock:
+            cert = ssock.getpeercert()
+            return cert
